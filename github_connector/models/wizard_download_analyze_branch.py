e776434b46366fe7057705237c8be6f04704670d
Sylvain LE GAL <sylvain.legal@grap.coop>
2017-07-21 14:20:09 +0200

    [ADD] two modules from 8.0 WIP branch

---
+++ github_connector/models/wizard_download_analyze_branch.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2016-Today: Odoo Community Association (OCA)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import api, models, fields
+
+
+class WizardDownloadAnalyzeBranch(models.TransientModel):
+    _name = 'wizard.download.analyze.branch'
+
+    download_source_code = fields.Boolean(
+        string='Download Source Code', default=True)
+
+    analyze_source_code = fields.Boolean(
+        string='Analyze Source Code', default=True)
+
+    @api.multi
+    def apply(self):
+        repository_obj = self.env['github.repository.branch']
+        for wizard in self:
+            branches = repository_obj.browse(self._context['active_ids'])
+            if wizard.download_source_code:
+                branches.button_download_code()
+            if wizard.analyze_source_code:
+                branches.button_analyze_code()
