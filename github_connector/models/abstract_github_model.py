4e49b2311b7aa10f89ce370a842f8645b3d7a897
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2017-10-29 18:41:29 +0100

    [IMP] log error in case of multiple partners with same github id
    [IMP] raise error instead of logging in case of duplicates
    
    To avoid creating additional duplicates.

--- github_connector/models/abstract_github_model.py
+++ github_connector/models/abstract_github_model.py
@@ -7,7 +7,9 @@ import base64
 import urllib
 import logging
 
-from openerp import tools, api, fields, models, exceptions, _
+from odoo import tools, api, fields, models, exceptions, _
+from odoo.exceptions import UserError
+
 from .github import Github
 
 _logger = logging.getLogger(__name__)
@@ -131,16 +133,20 @@ class AbtractGithubModel(models.AbstractModel):
         if self._github_login_field and self._github_login_field in data:
             existing_object = self.search([
                 ('github_login', '=', data[self._github_login_field])])
-            if existing_object:
+            if len(existing_object) == 1:
                 # Update the existing object with the id
                 existing_object.github_id_external = data['id']
                 _logger.info(
                     "Existing object %s#%d with github name '%s' has been"
-                    " updated with unique github id %s#%s" % (
-                        self._name, existing_object.id,
-                        data[self._github_login_field], data['id'],
-                        self.github_type()))
+                    " updated with unique github id %s#%s",
+                    self._name, existing_object.id,
+                    data[self._github_login_field], data['id'],
+                    self.github_type())
                 return existing_object
+            elif len(existing_object) > 1:
+                raise UserError(
+                    _("Duplicate object with github login %s") %
+                    (data[self._github_login_field], ))
 
         if self._need_individual_call:
             github_connector = self.get_github_connector(self.github_type())
