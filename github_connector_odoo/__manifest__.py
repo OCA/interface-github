0096793f9641116844f6e4b4f1b527316aa1a6b8
cubells <vicent@vcubells.net>
2018-05-21 19:14:08 +0200

    [MIG] : Migration to 11.0

--- github_connector_odoo/__manifest__.py
+++ github_connector_odoo/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2016-Today: Odoo Community Association (OCA)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Github Connector - Odoo',
     'summary': 'Analyze Odoo modules information from Github repositories',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Connector',
     'license': 'AGPL-3',
     'author': 'Odoo Community Association (OCA), Sylvain LE GAL, GRAP',
