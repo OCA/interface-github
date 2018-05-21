9f3cbd1231fd29888b557890647837b55166e408
cubells <vicent@vcubells.net>
2018-05-21 19:14:08 +0200

    [MIG] : Migration to 11.0

--- github_connector_odoo/models/odoo_author.py
+++ github_connector_odoo/models/odoo_author.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2016-Today: Odoo Community Association (OCA)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api
+from odoo import api, fields, models
 
 
 class OdooAuthor(models.Model):
