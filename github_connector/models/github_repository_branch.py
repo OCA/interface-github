c6faf6753637b63e391c08909cc32c4ab37300d2
Stephan Rozendaal <stephan@west.nl>
2018-10-02 15:49:15 +0200

    [IMP] Fix error in analyze call
    Fix a programming mistake in the analyze call.
    By @RoelAdriaans

--- github_connector/models/github_repository_branch.py
+++ github_connector/models/github_repository_branch.py
@@ -224,18 +224,19 @@ class GithubRepository(models.Model):
                 _logger.info("Analyzing Source Code in %s ..." % path)
                 try:
                     vals = branch.analyze_code_one(branch)
+                    vals.update({
+                        'last_analyze_date': datetime.today(),
+                        'state': 'analyzed',
+                    })
+                    # Mark the branch as analyzed
+                    branch.write(vals)
+                    if partial_commit:
+                        self._cr.commit()  # pylint: disable=invalid-commit
+
                 except Exception as e:
                     _logger.warning(
                         'Cannot analyze branch %s so skipping it, error '
-                        'is: %s' % (branch.name, e.message))
-                vals.update({
-                    'last_analyze_date': datetime.today(),
-                    'state': 'analyzed',
-                })
-                # Mark the branch as analyzed
-                branch.write(vals)
-                if partial_commit:
-                    self._cr.commit()  # pylint: disable=invalid-commit
+                        'is: %s' % (branch.name, e))
         return True
 
     # Compute Section
