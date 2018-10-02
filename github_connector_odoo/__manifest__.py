41e7a56343f49a1ccacfd60611f0a1026721277d
Stephan Rozendaal <stephan@west.nl>
2018-10-02 16:28:43 +0200

    [IMP] Add try-except on branch analyze action (#26)
    * [IMP] Add try-except on branch analyze action
    
    Add try-except statements on analyze actions that could
    potentially crash the analyze process.
    
    * [REF] Fix error logging
    
    Changes include:
     - fix error values
     - use error instead of warning
    
    * [IMP] Fix error in analyze call
    
    Fix a programming mistake in the analyze call.
    By @RoelAdriaans
    
    * [IMP] Bump version number of github_connector
    
    Bump the version number of github_connector to '11.0.1.1.1'.
    
    * [IMP] Bump version number of github_connector
    
    Bump the version number of github_connector_odoo to
    '11.0.1.1.1'.

--- github_connector_odoo/__manifest__.py
+++ github_connector_odoo/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Github Connector - Odoo',
     'summary': 'Analyze Odoo modules information from Github repositories',
-    'version': '11.0.1.1.0',
+    'version': '11.0.1.1.1',
     'category': 'Connector',
     'license': 'AGPL-3',
     'author': 'Odoo Community Association (OCA), Sylvain LE GAL, GRAP',
