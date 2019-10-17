edd95bc91792709fee0b972e847caa8823d377ff
Jordi Riera <jordi.riera@numigi.com>
2019-10-17 15:27:55 +0200

    Linter

--- github_connector/models/github.py
+++ github_connector/models/github.py
@@ -183,6 +183,7 @@ class Github(object):
         return self.get_by_url(url, 'get')
 
     def create(self, arguments, data):
+        # pylint: disable=method-required-super
         url = self._build_url(arguments, 'url_create', None)
         res = self.get_by_url(url, 'post', data)
         return res
