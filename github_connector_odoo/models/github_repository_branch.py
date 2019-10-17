177c6086fe8805625cfc74cbcdd51335a1b868e8
Jordi Riera <jordi.riera@numigi.com>
2019-10-17 22:18:53 +0200

    fix list_dir after change in signature

--- github_connector_odoo/models/github_repository_branch.py
+++ github_connector_odoo/models/github_repository_branch.py
@@ -129,7 +129,7 @@ class GithubRepositoryBranch(models.Model):
                 if os.path.isfile(opj(folder, name, mname)):
                     return True
 
-        return map(clean, filter(is_really_module, os.listdir(dir)))
+        return map(clean, filter(is_really_module, os.listdir(folder)))
 
     def _analyze_module_name(self, path, module_name):
         self.ensure_one()
