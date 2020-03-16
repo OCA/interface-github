ec41edb601fa591a1b49ccb6dcd38460137790d3
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2020-03-16 12:10:27 -0400

    [IMP] github_connector: black, isort

--- github_connector/models/github_organization_serie.py
+++ github_connector/models/github_organization_serie.py
@@ -6,20 +6,26 @@ from odoo import fields, models
 
 
 class GithubOrganizationSerie(models.Model):
-    _name = 'github.organization.serie'
-    _description = 'Github Organization Serie'
-    _order = 'sequence, name'
+    _name = "github.organization.serie"
+    _description = "Github Organization Serie"
+    _order = "sequence, name"
 
     # Columns Section
-    name = fields.Char(string='Name', required=True)
+    name = fields.Char(string="Name", required=True)
 
-    sequence = fields.Integer(string='Sequence', required=True)
+    sequence = fields.Integer(string="Sequence", required=True)
 
     organization_id = fields.Many2one(
-        comodel_name='github.organization', string='Organization',
-        ondelete='cascade', required=True)
+        comodel_name="github.organization",
+        string="Organization",
+        ondelete="cascade",
+        required=True,
+    )
 
     _sql_constraints = [
-        ('sequence_organization_uniq', 'unique(organization_id, sequence)',
-         "Sequence serie must be unique by organization.")
+        (
+            "sequence_organization_uniq",
+            "unique(organization_id, sequence)",
+            "Sequence serie must be unique by organization.",
+        )
     ]
