2a5f3870e31c93a0de5521f628e6ef0ebd68def4
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2020-03-16 12:11:32 -0400

    [MIG] github_connector: Migration to 13.0

--- github_connector/models/github_organization.py
+++ github_connector/models/github_organization.py
@@ -9,6 +9,7 @@ class GithubOrganization(models.Model):
     _name = "github.organization"
     _inherit = ["abstract.github.model"]
     _order = "name"
+    _description = "Github organization"
 
     _github_type = "organization"
     _github_login_field = "login"
@@ -16,7 +17,7 @@ class GithubOrganization(models.Model):
     # Columns Section
     name = fields.Char(string="Organization Name", required=True, readonly=True)
 
-    image = fields.Binary(string="Image", readonly=True)
+    image = fields.Image(string="Image", readonly=True)
 
     description = fields.Char(string="Description", readonly=True)
 
@@ -89,7 +90,7 @@ class GithubOrganization(models.Model):
     # Overloadable Section
     @api.model
     def get_conversion_dict(self):
-        res = super(GithubOrganization, self).get_conversion_dict()
+        res = super().get_conversion_dict()
         res.update(
             {
                 "name": "name",
@@ -103,14 +104,11 @@ class GithubOrganization(models.Model):
 
     @api.model
     def get_odoo_data_from_github(self, data):
-        res = super(GithubOrganization, self).get_odoo_data_from_github(data)
+        res = super().get_odoo_data_from_github(data)
         if "avatar_url" in data:
-            res.update(
-                {"image": self.get_base64_image_from_github(data["avatar_url"]),}
-            )
+            res.update({"image": self.get_base64_image_from_github(data["avatar_url"])})
         return res
 
-    @api.multi
     def full_update(self):
         self.button_sync_member()
         self.button_sync_repository()
@@ -124,25 +122,21 @@ class GithubOrganization(models.Model):
         return True
 
     # Compute Section
-    @api.multi
     @api.depends("member_ids", "member_ids.organization_ids")
     def _compute_member_qty(self):
         for organization in self:
             organization.member_qty = len(organization.member_ids)
 
-    @api.multi
     @api.depends("repository_ids.organization_id")
     def _compute_repository_qty(self):
         for organization in self:
             organization.repository_qty = len(organization.repository_ids)
 
-    @api.multi
     @api.depends("team_ids.organization_id")
     def _compute_team_qty(self):
         for organization in self:
             organization.team_qty = len(organization.team_ids)
 
-    @api.multi
     @api.depends("organization_serie_ids.organization_id")
     def _compute_organization_serie_qty(self):
         for organization in self:
@@ -151,7 +145,6 @@ class GithubOrganization(models.Model):
             )
 
     # Action section
-    @api.multi
     def button_sync_member(self):
         github_member = self.get_github_connector("organization_members")
         partner_obj = self.env["res.partner"]
@@ -162,7 +155,6 @@ class GithubOrganization(models.Model):
                 member_ids.append(partner.id)
             organization.member_ids = member_ids
 
-    @api.multi
     def button_sync_repository(self):
         repository_obj = self.env["github.repository"]
         github_repo = self.get_github_connector("organization_repositories")
@@ -173,7 +165,6 @@ class GithubOrganization(models.Model):
                 repository_ids.append(repository.id)
             organization.repository_ids = repository_ids
 
-    @api.multi
     def button_sync_team(self):
         team_obj = self.env["github.team"]
         github_team = self.get_github_connector("organization_teams")
@@ -185,3 +176,27 @@ class GithubOrganization(models.Model):
                 )
                 team_ids.append(team.id)
             organization.team_ids = team_ids
+
+    def action_github_repository(self):
+        self.ensure_one()
+        action = self.env.ref("github_connector.action_github_repository").read()[0]
+        action["context"] = dict(self.env.context)
+        action["context"].pop("group_by", None)
+        action["context"]["search_default_organization_id"] = self.id
+        return action
+
+    def action_github_team(self):
+        self.ensure_one()
+        action = self.env.ref("github_connector.action_github_team").read()[0]
+        action["context"] = dict(self.env.context)
+        action["context"].pop("group_by", None)
+        action["context"]["search_default_organization_id"] = self.id
+        return action
+
+    def action_res_partner(self):
+        self.ensure_one()
+        action = self.env.ref("github_connector.action_res_partner").read()[0]
+        action["context"] = dict(self.env.context)
+        action["context"].pop("group_by", None)
+        action["context"]["search_default_organization_ids"] = self.id
+        return action
