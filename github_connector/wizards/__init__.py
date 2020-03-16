d159e9949626a86103b773365785d7eaf070e6e0
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2020-03-16 12:11:32 -0400

    [MIG] github_connector: Migration to 13.0

--- github_connector/wizards/__init__.py
+++ github_connector/wizards/__init__.py
@@ -1,7 +1,3 @@
 from . import wizard_create_team
 from . import wizard_create_repository
-
 from . import wizard_load_github_model
-from . import wizard_download_analyze_branch
-from . import wizard_update_from_github
-from . import wizard_update_branch_list
