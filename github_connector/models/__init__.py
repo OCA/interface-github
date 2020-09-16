7cd69240647a4ee1c2b60c35070d821ed22113a9
Víctor Martínez <victor.martinez@tecnativa.com>
2020-09-16 12:13:10 +0200

    [IMP] github_connector: Code analysis

--- github_connector/models/__init__.py
+++ github_connector/models/__init__.py
@@ -3,6 +3,9 @@ from . import abstract_github_model
 
 from . import res_partner
 
+from . import github_analysis_rule
+from . import github_analysis_rule_group
+from . import github_analysis_rule_info
 from . import github_organization
 from . import github_organization_serie
 from . import github_repository
