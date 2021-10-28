be3d82fe879f9cf7db0812c2eed1007072f491d0
Miquel Raïch <miquel.raich@forgeflow.com>
2021-10-28 10:29:40 +0200

    [FIX] github_connector: migration scripts

--- github_connector/migrations/14.0.1.0.0/pre-migration.py
+++ github_connector/migrations/14.0.1.0.0/pre-migration.py
@@ -2,29 +2,22 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from openupgradelib import openupgrade
 
-column_renames = {
-    "res_partner": [
-        ("github_team_ids", "github_team_partner_ids"),
-        ("github_login", "github_name"),
-    ],
-    "github_organization": [
-        ("github_login", "github_name"),
-    ],
-    "github_repository_branch": [
-        ("github_login", "github_name"),
-    ],
-    "github_repository": [
-        ("github_login", "github_name"),
-    ],
-    "github_team_partner": [
-        ("github_login", "github_name"),
-    ],
-    "github_team": [
-        ("github_login", "github_name"),
-    ],
-}
+field_renames = [
+    ("res.partner", "res_partner", "github_team_ids", "github_team_partner_ids"),
+    ("res.partner", "res_partner", "github_login", "github_name"),
+    ("github.organization", "github_organization", "github_login", "github_name"),
+    (
+        "github.repository.branch",
+        "github_repository_branch",
+        "github_login",
+        "github_name",
+    ),
+    ("github.repository", "github_repository", "github_login", "github_name"),
+    ("github.team.partner", "github_team_partner", "github_login", "github_name"),
+    ("github.team", "github_team", "github_login", "github_name"),
+]
 
 
 @openupgrade.migrate()
 def migrate(env, version):
-    openupgrade.rename_columns(env.cr, column_renames)
+    openupgrade.rename_fields(env, field_renames)
