62fd6d23fbf0222a114387af39d58f73950360b6
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2023-03-23 15:33:49 +0100

    [MIG] github_connector_oca: Migration to 15.0

--- github_connector_oca/__manifest__.py
+++ github_connector_oca/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Github Connector - OCA extension",
     "summary": "Add OCA specific information to Odoo modules",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Connector",
     "license": "AGPL-3",
     "author": "Odoo Community Association (OCA), Road-Support",
