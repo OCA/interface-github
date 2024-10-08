a4e077c2126e123a1ba84910958cb42a0fc0040b
Rivo Lalaina <rivo2302@gmail.com>
2024-10-08 14:54:43 +0300

    TA#69432 [FIX] github_connector :  access error

--- github_connector/models/github_repository.py
+++ github_connector/models/github_repository.py
@@ -4,7 +4,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import logging
-
 from github.GithubException import GithubException
 
 from odoo import api, fields, models
@@ -142,7 +141,7 @@ class GithubRepository(models.Model):
         try:
             gh_api = self.get_github_connector()
             return gh_api.get_repo(int(obj_id or self.github_id_external))
-        except GithubException as e:g
+        except GithubException as e:
             _logger.error(
                 "An error occurred while trying to fetch the repository %s: %s",
                 self.complete_name,
